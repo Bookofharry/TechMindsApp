3b3df3805bcfbb228c939896360d5b33e09b4988
DEV HARRY👌 <Bookofharry@hotmail.com>
2025-10-03 15:29:11 +0100

    Adjusted the Progress Bar3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import BusinessCtaSection from "../compo/cta"
+
 import HeroSplit from "../compo/SkillCard"
 import DownloadCta from "../compo/cta2"
+import Learn from "../compo/Learn"
 function Home(){
     return(
        <div> 
+        <Learn />
         <BusinessCtaSection />
         <HeroSplit />
         <DownloadCta />
